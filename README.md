51c1fa9b688af9d4ab1f126e701a2a7159c761a7
Rafaela Nascimento <93143251+rafanascm@users.noreply.github.com>
2024-12-19 15:35:31 +0000

    Adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Implementação do https://www.tabnews.com.br a partir de estudos do curso https://curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
-function Home(){
-    return <h1>Thays, eu amo você. Se você me ama, da um sorrisinho 😄💖</h1>
+function Home() {
+  return <h1>Thays, eu amo você. Se você me ama, da um sorrisinho 😄💖</h1>;
+}
+
+function teste() {
+  console.log("teste");
+  console.log("identação errada");
 }
 
 export default Home;
