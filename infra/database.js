c4c7b603f9ed3d9a3bf9a83eb1862368056e0017
Rafaela Nascimento <93143251+rafanascm@users.noreply.github.com>
2025-01-17 09:56:06 -0300

    Adiciono sll conexão

--- infra/database.js
+++ infra/database.js
@@ -7,8 +7,9 @@ async function query(queryObject) {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
+    ssl: process.env.NODE_ENV === "development" ? false : true,
   });
-  console.log("Credencias do Postgres:", {
+  console.log("Credenciais do Postgres:", {
     host: process.env.POSTGRES_HOST,
     port: process.env.POSTGRES_PORT,
     user: process.env.POSTGRES_USER,
@@ -20,7 +21,7 @@ async function query(queryObject) {
     const result = await client.query(queryObject);
     return result;
   } catch (error) {
-    console.error(error);
+    console.error("Erro ao executar a consulta:", error);
     throw error;
   } finally {
     await client.end();
